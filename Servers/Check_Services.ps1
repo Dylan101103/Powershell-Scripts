db554bb9fe15efa8a7cce6a4463a8a939eec8a6b
Dylan101103 <28712778+Dylan101103@users.noreply.github.com>
2018-01-03 20:59:02 -0500

    Add files via upload

---
+++ Servers/Check_Services.ps1
@@ -0,0 +1,16 @@
+﻿$ServiceName = 'Serenade'
+$arrService = Get-Service -Name $ServiceName
+
+if ($arrService.Status -ne 'Running'){
+$ServiceStarted = $false}
+Else{$ServiceStarted = $true}
+
+while ($ServiceStarted -ne $true){
+Start-Service $ServiceName
+write-host $arrService.status
+write-host 'Service started'
+Start-Sleep -seconds 60
+$arrService = Get-Service -Name $ServiceName #Why is this line needed?
+if ($arrService.Status -eq 'Running'){
+$ServiceStarted = $true}
+}
